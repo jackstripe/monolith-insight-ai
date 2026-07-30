a0611e9844f3230fb4a1f615ba53a89508510672
Cesar Flores Lovera <ingcesarfl@gmail.com>
2026-07-30 12:12:05 -0600

    Least coupled classes. code and testing

---
+++ src/test/java/com/monolithinsight/application/FindLeastCoupledClassesUseCaseTest.java
@@ -0,0 +1,224 @@
+package com.monolithinsight.application;
+
+import com.monolithinsight.domain.*;
+import com.monolithinsight.support.TestFixtures;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class FindLeastCoupledClassesUseCaseTest {
+
+
+    @Test
+    void shouldCalculateMostCoupledClasses() {
+        ClassNode orderService =
+                ClassNode.from(
+                        TestFixtures.createClass(
+                                "com.example",
+                                "OrderService"
+                        )
+                );
+
+        ClassNode orderRepository =
+                ClassNode.from(
+                        TestFixtures.createClass(
+                                "com.example",
+                                "OrderRepository"
+                        )
+                );
+
+        ClassNode orderController =
+                ClassNode.from(
+                        TestFixtures.createClass(
+                                "com.example",
+                                "OrderController"
+                        )
+                );
+
+        ProjectGraph graph = new ProjectGraph(
+                List.of(
+                        orderService,
+                        orderRepository,
+                        orderController
+                ),
+                List.of(
+                        new ClassDependency(
+                                orderController.id(),
+                                orderService.id(),
+                                DependencyType.FIELD
+                        ),
+                        new ClassDependency(
+                                orderService.id(),
+                                orderRepository.id(),
+                                DependencyType.CONSTRUCTOR
+                        )
+                )
+        );
+
+        GraphMetrics metrics =
+                new AnalyzeGraphMetricsUseCase()
+                        .execute(graph);
+
+        CouplingReport report = new FindLeastCoupledClassesUseCase().execute(metrics,2);
+
+        assertThat(report.classes())
+                .extracting(
+                        CoupledClass::classId,
+                        CoupledClass::incomingDependencies,
+                        CoupledClass::outgoingDependencies,
+                        CoupledClass::totalDependencies
+                )
+                .containsExactly(
+                        tuple("com.example.OrderController",
+                                0,
+                                1,
+                                1),
+                        tuple("com.example.OrderRepository",
+                                1,
+                                0,
+                                1)
+
+
+                );
+    }
+
+    @Test
+    void shouldCalculateMostCoupledClassesLimitGreaterThanSize() {
+        ClassNode orderService =
+                ClassNode.from(
+                        TestFixtures.createClass(
+                                "com.example",
+                                "OrderService"
+                        )
+                );
+
+        ClassNode orderRepository =
+                ClassNode.from(
+                        TestFixtures.createClass(
+                                "com.example",
+                                "OrderRepository"
+                        )
+                );
+
+        ClassNode orderController =
+                ClassNode.from(
+                        TestFixtures.createClass(
+                                "com.example",
+                                "OrderController"
+                        )
+                );
+
+        ProjectGraph graph = new ProjectGraph(
+                List.of(
+                        orderService,
+                        orderRepository,
+                        orderController
+                ),
+                List.of(
+                        new ClassDependency(
+                                orderController.id(),
+                                orderService.id(),
+                                DependencyType.FIELD
+                        ),
+                        new ClassDependency(
+                                orderService.id(),
+                                orderRepository.id(),
+                                DependencyType.CONSTRUCTOR
+                        )
+                )
+        );
+
+        GraphMetrics metrics =
+                new AnalyzeGraphMetricsUseCase()
+                        .execute(graph);
+
+        CouplingReport report = new FindLeastCoupledClassesUseCase().execute(metrics,4);
+
+        assertThat(report.classes())
+                .extracting(
+                        CoupledClass::classId,
+                        CoupledClass::incomingDependencies,
+                        CoupledClass::outgoingDependencies,
+                        CoupledClass::totalDependencies
+                )
+                .containsExactly(
+                        tuple("com.example.OrderController",
+                                0,
+                                1,
+                                1),
+                        tuple("com.example.OrderRepository",
+                                1,
+                                0,
+                                1),
+                        tuple("com.example.OrderService",
+                                1,
+                                1,
+                                2)
+
+                );
+    }
+
+    @Test
+    void shouldCalculateMostCoupledClassesLimitIsZero() {
+        ClassNode orderService =
+                ClassNode.from(
+                        TestFixtures.createClass(
+                                "com.example",
+                                "OrderService"
+                        )
+                );
+
+        ClassNode orderRepository =
+                ClassNode.from(
+                        TestFixtures.createClass(
+                                "com.example",
+                                "OrderRepository"
+                        )
+                );
+
+        ClassNode orderController =
+                ClassNode.from(
+                        TestFixtures.createClass(
+                                "com.example",
+                                "OrderController"
+                        )
+                );
+
+        ProjectGraph graph = new ProjectGraph(
+                List.of(
+                        orderService,
+                        orderRepository,
+                        orderController
+                ),
+                List.of(
+                        new ClassDependency(
+                                orderController.id(),
+                                orderService.id(),
+                                DependencyType.FIELD
+                        ),
+                        new ClassDependency(
+                                orderService.id(),
+                                orderRepository.id(),
+                                DependencyType.CONSTRUCTOR
+                        )
+                )
+        );
+
+        GraphMetrics metrics =
+                new AnalyzeGraphMetricsUseCase()
+                        .execute(graph);
+
+        CouplingReport report = new FindLeastCoupledClassesUseCase().execute(metrics,0);
+
+        assertThat(report.classes())
+                .extracting(
+                        CoupledClass::classId,
+                        CoupledClass::incomingDependencies,
+                        CoupledClass::outgoingDependencies,
+                        CoupledClass::totalDependencies
+                ).isEmpty();
+    }
+}
