cbc640b323a8ba7c1ae407fe9015701de7a81cbe
Cesar Flores Lovera <ingcesarfl@gmail.com>
2026-07-30 15:28:32 -0600

    feat: calculate package dependency counts

--- src/main/java/com/monolithinsight/application/AnalyzePackageMetricsUseCase.java
+++ src/main/java/com/monolithinsight/application/AnalyzePackageMetricsUseCase.java
@@ -1,14 +1,67 @@
 package com.monolithinsight.application;
 
-import com.monolithinsight.domain.PackageMetricsReport;
-import com.monolithinsight.domain.ProjectGraph;
+import com.monolithinsight.domain.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AnalyzePackageMetricsUseCase {
 
     public PackageMetricsReport execute(ProjectGraph graph) {
-    // implementación
-        graph.nodes()
-        return null;
+
+
+        Map<String, Integer> incomingByPackage = new HashMap<>();
+        Map<String, Integer> outgoingByPackage = new HashMap<>();
+
+
+
+
+        Map<String, String> packageByClassId =
+                graph.nodes().stream()
+                        .collect(Collectors.toMap(
+                                ClassNode::id,
+                                node -> node.classInfo().packageName()
+                        ));
+
+        Map<String, Integer> classCountByPackage =
+                graph.nodes().stream()
+                        .collect(Collectors.groupingBy(
+                                node -> node.classInfo().packageName(),
+                                Collectors.summingInt(node -> 1)
+                        ));
+
+        for(ClassDependency dependency : graph.dependencies()){
+            String sourcePackage = packageByClassId.get(dependency.sourceNodeId());
+
+            String targetPackage = packageByClassId.get(dependency.targetNodeId());
+
+            if (sourcePackage.equals(targetPackage)) {
+                continue;
+            }
+            outgoingByPackage.merge(
+                    sourcePackage,
+                    1,
+                    Integer::sum
+            );
+
+            incomingByPackage.merge(
+                    targetPackage,
+                    1,
+                    Integer::sum
+            );
+        }
+
+        List<PackageMetrics> packages = new ArrayList<>();
+
+        classCountByPackage.forEach((key, value) ->
+                packages.add(new PackageMetrics(
+                    key,
+                    classCountByPackage.getOrDefault(key,0),
+                    incomingByPackage.getOrDefault(key,0),
+                    outgoingByPackage.getOrDefault(key,0))));
+
+        packages.sort(Comparator.comparing(PackageMetrics::packageName));
+        return new PackageMetricsReport(packages);
 
     }
 }
---
+++ src/test/java/com/monolithinsight/application/AnalyzePackageMetricsUseCaseTest.java
@@ -0,0 +1,4 @@
+package com.monolithinsight.application;
+
+public class AnalyzePackageMetricsUseCaseTest {
+}
