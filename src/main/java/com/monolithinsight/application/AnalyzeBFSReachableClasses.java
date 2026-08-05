f4db3b6682f91f6f28b9dc8a12ec81fc7106b176
Cesar Flores Lovera <ingcesarfl@gmail.com>
2026-08-05 15:15:22 -0600

    First analysis and creation of the structure to review. This doesnt contemplate the logic of the BFS immplemented v2

--- src/main/java/com/monolithinsight/application/AnalyzeBFSReachableClasses.java
+++ src/main/java/com/monolithinsight/application/AnalyzeBFSReachableClasses.java
@@ -2,13 +2,15 @@ package com.monolithinsight.application;
 
 import com.monolithinsight.domain.ProjectGraph;
 
+import java.util.List;
+
 public class AnalyzeBFSReachableClasses {
 
 
 
     public ReachabilityReport execute(ProjectGraph graph, String classId){
 
+        return new ReachabilityReport(List.of());
 
-        return null;
     }
 }
