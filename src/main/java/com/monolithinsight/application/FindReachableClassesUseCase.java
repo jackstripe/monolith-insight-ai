951e0f6c9ed4c1493c9881778b25236d643a893b
Cesar Flores Lovera <ingcesarfl@gmail.com>
2026-08-05 16:45:07 -0600

    Added functionality to BFS adjacency list. TEsts are passing on green

--- src/main/java/com/monolithinsight/application/FindReachableClassesUseCase.java
+++ src/main/java/com/monolithinsight/application/FindReachableClassesUseCase.java
@@ -1,9 +1,13 @@
 package com.monolithinsight.application;
 
+import com.monolithinsight.domain.ClassDependency;
+import com.monolithinsight.domain.ClassNode;
 import com.monolithinsight.domain.ProjectGraph;
 import com.monolithinsight.domain.ReachabilityReport;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class FindReachableClassesUseCase {
 
@@ -11,7 +15,55 @@ public class FindReachableClassesUseCase {
 
     public ReachabilityReport execute(ProjectGraph graph, String classId){
 
-        return new ReachabilityReport(List.of());
+        Map<String, ClassNode> nodesById;
+        Map<String, List<String>> adjacencyList;
+        Set<String> visited =  new HashSet<>();
+        Queue<String> pending = new ArrayDeque<>();
+
+        nodesById = graph.nodes().stream()
+                .collect(Collectors.toMap(
+                        ClassNode::id,
+                        Function.identity()
+                ));
+        adjacencyList = graph.dependencies().stream()
+                .collect(Collectors.groupingBy(
+                        ClassDependency::sourceNodeId,
+                        Collectors.mapping(ClassDependency::targetNodeId, Collectors.toList())
+                        )
+                );
+
+        visited.add(classId);
+        pending.add(classId);
+
+        while(!pending.isEmpty()){
+            for(String adjacents : pending){
+                List<String> list = adjacencyList.get(adjacents);
+                if(list != null){
+                    for(String adjacent :  adjacencyList.getOrDefault(adjacents,Collections.emptyList())){
+                        pending.remove();
+                        if (visited.add(adjacent)) {
+                            pending.add(adjacent);
+                        }
+                    }
+                }else {
+                    pending.remove();
+                }
+
+            }
+        }
+
+        visited.remove(classId);
+        ReachabilityReport reachabilityReport =  new ReachabilityReport(nodesById.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .filter(
+                        entry -> visited.contains(entry.getKey())
+                )
+                .map(Map.Entry::getValue)
+                .toList());
+
+
+
+        return reachabilityReport;
 
     }
 }
