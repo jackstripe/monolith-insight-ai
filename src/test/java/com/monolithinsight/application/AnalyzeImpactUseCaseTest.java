99c27c827397c8f4e3adb9a461b5e44dc03195c6
Cesar Flores Lovera <ingcesarfl@gmail.com>
2026-08-08 02:06:05 -0600

    Added reamining tesst

--- src/test/java/com/monolithinsight/application/AnalyzeImpactUseCaseTest.java
+++ src/test/java/com/monolithinsight/application/AnalyzeImpactUseCaseTest.java
@@ -6,12 +6,14 @@ import com.monolithinsight.domain.ProjectGraph;
 import com.monolithinsight.support.ProjectGraphTestBuilder;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-public class AnalyzeImpactUseCaseTest {
+class AnalyzeImpactUseCaseTest {
 
     @Test
     void shouldFindDirectAndTransitiveImpactedClasses() {
+
         ProjectGraph graph = new ProjectGraphTestBuilder()
                 .addNode("OrderController")
                 .addNode("BatchProcessor")
@@ -31,6 +33,68 @@ public class AnalyzeImpactUseCaseTest {
                 .extracting(ClassNode::id)
                 .containsExactly(
                         "BatchProcessor",
+                        "OrderController",
+                        "OrderService"
+                );
+    }
+
+    @Test
+    void shouldReturnEmptyReportWhenNoClassesDependOnChangedClass(){
+
+        ProjectGraph graph = new ProjectGraphTestBuilder()
+                .addNode("OrderController")
+                .addDependency("OrderController", "OrderService")
+                .build();
+
+        ImpactAnalysisReport report =
+                new AnalyzeImpactUseCase().execute(graph, "OrderController");
+
+        assertThat(report.impactedClasses())
+                .extracting(ClassNode::id)
+                .isEmpty();
+    }
+
+    @Test
+    void shouldRejectUnknownChangedClass() {
+
+        ProjectGraph graph = new ProjectGraphTestBuilder()
+                .addNode("OrderController")
+                .addNode("BatchProcessor")
+                .addNode("OrderService")
+                .addNode("AuditService")
+                .addNode("LegacyHelper")
+                .addDependency("OrderController", "OrderService")
+                .addDependency("BatchProcessor", "OrderService")
+                .addDependency("OrderService", "AuditService")
+                .addDependency("AuditService", "LegacyHelper")
+                .build();
+
+        assertThatThrownBy(() ->
+                new FindReachableClassesUseCase()
+                        .execute(graph, "UnkownService")
+        )
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage( "Class not found in graph: " + "UnkownService"
+                );
+    }
+
+    @Test
+    void shouldHandleCycleWithoutIncludingChangedClass() {
+        ProjectGraph graph = new ProjectGraphTestBuilder()
+                .addNode("OrderController")
+                .addNode("BatchProcessor")
+                .addNode("OrderService")
+                .addDependency("OrderController", "BatchProcessor")
+                .addDependency("BatchProcessor", "OrderService")
+                .addDependency("OrderService", "OrderController")
+                .build();
+
+        ImpactAnalysisReport report =
+                new AnalyzeImpactUseCase().execute(graph, "BatchProcessor");
+
+        assertThat(report.impactedClasses())
+                .extracting(ClassNode::id)
+                .containsExactly(
                         "OrderController",
                         "OrderService"
                 );
