56f9d07e92d41eb207482386feadeb08c79df962
Cesar Flores Lovera <ingcesarfl@gmail.com>
2026-08-08 01:49:30 -0600

    added the logic for the BFS graph tree creating. Creation of the Analysis impact REport

--- src/main/java/com/monolithinsight/application/AnalyzeImpactUseCase.java
+++ src/main/java/com/monolithinsight/application/AnalyzeImpactUseCase.java
@@ -5,8 +5,7 @@ import com.monolithinsight.domain.ClassNode;
 import com.monolithinsight.domain.ImpactAnalysisReport;
 import com.monolithinsight.domain.ProjectGraph;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -16,6 +15,10 @@ public class AnalyzeImpactUseCase {
             ProjectGraph graph,
             String changedClassId
     ) {
+
+        Set<String> visited =  new HashSet<>();
+        Queue<String> pending = new ArrayDeque<>();
+
         Map<String, ClassNode> nodesById =
                 graph.nodes().stream()
                         .collect(Collectors.toMap(
@@ -38,6 +41,30 @@ public class AnalyzeImpactUseCase {
                                 )
                         ));
 
-        return new ImpactAnalysisReport(List.of());
+        visited.add(changedClassId);
+        pending.add(changedClassId);
+
+
+        while(!pending.isEmpty()){
+
+            String currentId = pending.remove();
+
+            for(String adjacentClassId : reverseAdjacencyList.getOrDefault(currentId, Collections.emptyList())){
+                if (visited.add(adjacentClassId)) {
+                    pending.add(adjacentClassId);
+                }
+            }
+        }
+
+        visited.remove(changedClassId);
+
+        List<ClassNode> impactedClasses = visited
+                .stream()
+                .sorted()
+                .map(nodesById::get)
+                .toList();
+
+        return new ImpactAnalysisReport(impactedClasses);
+
     }
 }
--- src/main/java/com/monolithinsight/domain/ImpactAnalysisReport.java
+++ src/main/java/com/monolithinsight/domain/ImpactAnalysisReport.java
@@ -2,11 +2,12 @@ package com.monolithinsight.domain;
 
 import java.util.List;
 
-public record ImpactAnalysisReport (List<ClassNode> impactedClasses) {
+public record ImpactAnalysisReport (
+        List<ClassNode> impactedClasses
+) {
 
     public ImpactAnalysisReport {
 
         impactedClasses = List.copyOf(impactedClasses);
     }
 }
-
