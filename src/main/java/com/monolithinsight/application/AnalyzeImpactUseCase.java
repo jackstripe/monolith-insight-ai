205d94bb334ce3b3af0a2e706d6bdeec8e50b5e1
Cesar Flores Lovera <ingcesarfl@gmail.com>
2026-08-07 21:25:38 -0600

    Added creation of indexes and te adjacency list

--- src/main/java/com/monolithinsight/application/AnalyzeImpactUseCase.java
+++ src/main/java/com/monolithinsight/application/AnalyzeImpactUseCase.java
@@ -1,16 +1,43 @@
 package com.monolithinsight.application;
 
+import com.monolithinsight.domain.ClassDependency;
+import com.monolithinsight.domain.ClassNode;
 import com.monolithinsight.domain.ImpactAnalysisReport;
 import com.monolithinsight.domain.ProjectGraph;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-class AnalyzeImpactUseCase {
+public class AnalyzeImpactUseCase {
 
     public ImpactAnalysisReport execute(
             ProjectGraph graph,
             String changedClassId
     ) {
+        Map<String, ClassNode> nodesById =
+                graph.nodes().stream()
+                        .collect(Collectors.toMap(
+                                ClassNode::id,
+                                Function.identity()
+                        ));
+
+        if (!nodesById.containsKey(changedClassId)) {
+            throw new IllegalArgumentException(
+                    "Class not found in graph: " + changedClassId
+            );
+        }
+        Map<String, List<String>> reverseAdjacencyList =
+                graph.dependencies().stream()
+                        .collect(Collectors.groupingBy(
+                                ClassDependency::targetNodeId,
+                                Collectors.mapping(
+                                        ClassDependency::sourceNodeId,
+                                        Collectors.toList()
+                                )
+                        ));
+
         return new ImpactAnalysisReport(List.of());
     }
 }
