aba177b84add601afe04c93fb75ac2af37cf8db0
Cesar Flores Lovera <ingcesarfl@gmail.com>
2026-08-11 19:07:15 -0600

    feat: find all dependency paths using DFS

--- src/main/java/com/monolithinsight/application/FindDependencyPathsUseCase.java
+++ src/main/java/com/monolithinsight/application/FindDependencyPathsUseCase.java
@@ -1,9 +1,10 @@
 package com.monolithinsight.application;
 
-import com.monolithinsight.domain.DependencyPathsReport;
-import com.monolithinsight.domain.ProjectGraph;
+import com.monolithinsight.domain.*;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class FindDependencyPathsUseCase {
 
@@ -12,7 +13,94 @@ public class FindDependencyPathsUseCase {
             String sourceClassId,
             String targetClassId
     ){
+        List<DependencyPath> paths = new ArrayList<>();
+        List<String> currentPath = new ArrayList<>();
+        Set<String> classesInCurrentPath = new HashSet<>();
 
-        return new DependencyPathsReport(List.of());
+        Map<String, ClassNode> nodesById =
+                graph.nodes().stream()
+                        .collect(Collectors.toMap(
+                                ClassNode::id,
+                                Function.identity()
+                        ));
+
+        if (!nodesById.containsKey(sourceClassId)) {
+            throw new IllegalArgumentException(
+                    "Class not found in graph: " + sourceClassId
+            );
+        }
+
+        if (!nodesById.containsKey(targetClassId)) {
+            throw new IllegalArgumentException(
+                    "Class not found in graph: " + targetClassId
+            );
+        }
+
+        Map<String, List<String>> adjacencyList =
+                graph.dependencies().stream()
+                        .collect(Collectors.groupingBy(
+                                ClassDependency::sourceNodeId,
+                                Collectors.mapping(
+                                        ClassDependency::targetNodeId,
+                                        Collectors.collectingAndThen(
+                                                Collectors.toSet(),
+                                                targets -> targets.stream()
+                                                        .sorted()
+                                                        .toList()
+                                        )
+                                )
+                        ));
+
+        findPaths(
+                sourceClassId,
+                targetClassId,
+                adjacencyList,
+                nodesById,
+                currentPath,
+                classesInCurrentPath,
+                paths
+        );
+        return new DependencyPathsReport(paths);
     }
+
+    private void findPaths(
+            String currentClassId,
+            String targetClassId,
+            Map<String, List<String>> adjacencyList,
+            Map<String, ClassNode> nodesById,
+            List<String> currentPath,
+            Set<String> classesInCurrentPath,
+            List<DependencyPath> paths
+    ) {
+        currentPath.add(currentClassId);
+        classesInCurrentPath.add(currentClassId);
+
+        if (currentClassId.equals(targetClassId)) {
+            List<ClassNode> pathClasses = currentPath.stream()
+                    .map(nodesById::get)
+                    .toList();
+
+            paths.add(new DependencyPath(pathClasses));
+        } else {
+            for (String neighbour : adjacencyList.getOrDefault(currentClassId, Collections.emptyList())) {
+
+                if(!classesInCurrentPath.contains(neighbour)){
+
+                    findPaths(neighbour,
+                            targetClassId,
+                            adjacencyList,
+                            nodesById,
+                            currentPath,
+                            classesInCurrentPath,
+                            paths);
+                }
+
+                //classesInCurrentPath.remove(currentClassId);
+            }
+
+        }
+        currentPath.remove(currentPath.size() - 1);
+        classesInCurrentPath.remove(currentClassId);
+    }
+
 }
